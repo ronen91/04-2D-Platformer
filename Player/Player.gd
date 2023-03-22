7778b343919284ece10b8bcb934a1ccf55ecdc1b
ronen91 <122408058+ronen91@users.noreply.github.com>
2023-03-22 16:11:16 -0400

    complete exercise

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,6 @@
 extends KinematicBody2D
 
 onready var SM = $StateMachine
-onready var VP = get_viewport_rect()
 
 var velocity = Vector2.ZERO
 var jump_power = Vector2.ZERO
@@ -9,32 +8,36 @@ var direction = 1
 
 export var gravity = Vector2(0,30)
 
-export var move_speed = 20
-export var max_move = 300
+export var move_speed = 50
+export var max_move = 500
 
-export var jump_speed = 100
-export var max_jump = 1000
+export var jump_speed = 400
+export var max_jump = 1500
 
-export var leap_speed = 100
-export var max_leap = 1000
+export var leap_speed = 200
+export var max_leap = 1500
 
 var moving = false
 var is_jumping = false
-
-
-func _ready():
-	pass
+var double_jumped = false
+var should_direction_flip = true # wether or not player controls (left/right) can flip the player sprite
 
 
 func _physics_process(_delta):
 	velocity.x = clamp(velocity.x,-max_move,max_move)
-	
-	if direction < 0 and not $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = true
-	if direction > 0 and $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = false
-	
-	if position.y > Global.death_zone:
-		queue_free()
 		
+	if should_direction_flip:
+		if direction < 0 and not $AnimatedSprite.flip_h: 
+			$AnimatedSprite.flip_h = true
+			$Attack.cast_to.x = -1*abs($Attack.cast_to.x)
+		if direction > 0 and $AnimatedSprite.flip_h: 
+			$AnimatedSprite.flip_h = false
+			$Attack.cast_to.x = abs($Attack.cast_to.x)
+	
+	if is_on_floor():
+		double_jumped = false
+		if Input.is_action_just_pressed("attack"):
+			SM.set_state("Attacking")
 
 func is_moving():
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
@@ -55,5 +58,20 @@ func set_animation(anim):
 	if $AnimatedSprite.frames.has_animation(anim): $AnimatedSprite.play(anim)
 	else: $AnimatedSprite.play()
 
+func attack():
+	if $Attack.is_colliding():
+		var target = $Attack.get_collider()
+		if target.has_method("damage"):
+			target.damage()
+	if $Attack_low.is_colliding():
+		var target = $Attack_low.get_collider()
+		if target.has_method("damage"):
+			target.damage()
+
 func die():
 	queue_free()
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "Attacking":
+		SM.set_state("Idle")
